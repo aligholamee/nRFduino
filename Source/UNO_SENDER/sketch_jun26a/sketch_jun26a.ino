d9cce39e388be07c14f7159ea9eaa346c6ea4669
aligholami7596@gmail.com <aligholami7596@gmail.com>
2017-06-26 11:44:38 +0430

    add sender_duty

---
+++ Source/UNO_SENDER/sketch_jun26a/sketch_jun26a.ino
@@ -0,0 +1,22 @@
+/* nRF24L01+ Connection with two UNOs
+* ========================================
+* [] File Name : sketch_jun26a.c
+*
+* [] Duty: Send a "hello world" through nrf pipe
+* 
+* [] Creation Date : June 26, 2017
+*
+* [] Created By : Ali Gholami (aligholami7596@gmail.com)
+* ========================================
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
