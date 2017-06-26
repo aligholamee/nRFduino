ff7e33ed8dfdcf8a29f83da842d750213466b127
aligholami7596@gmail.com <aligholami7596@gmail.com>
2017-06-26 13:10:26 +0430

    USB setup on the receviver

---
+++ Source/UNO_RECEIVER/sketch_jun26b/sketch_jun26b.ino
@@ -0,0 +1,40 @@
+/* nRF24L01+ Connection with two UNOs
+* ========================================
+* [] File Name : sketch_jun26a.c
+*
+* [] Duty: Receive everything through the wireless pipe 
+* 
+* [] Creation Date : June 26, 2017
+*
+* [] Created By : Ali Gholami (aligholami7596@gmail.com)
+* ========================================
+*/
+
+#include <SPI.h> // Handles communication interface with the modem
+#include <nRF24L01.h> // Handles nRF24
+#include <RF24.h> // Some other controls over radio
+
+// RF24 Object
+// Represents a modem connected to the UNO
+// 7: CE Signal Port
+// 8: CSN Signal Port
+RF24 radio(7,8);
+
+// Receiver Address
+const byte rxAddr[6] = "00001";
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Set the serial communication of arduino and the computer
+  // Wait for the USB port switches to serial COM port until we connect USB cable
+  while (!Serial);
+  // Set the bps for USB
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
